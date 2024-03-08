51ce1e629eba38581c63f497d28fb800dfd2b2c7
humo_r1st <miklvizh@gmail.com>
2024-03-08 20:34:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+string allow = "abcdefghijklmnopqrstuvwxyz1234567890";
+string test;
+Console.WriteLine("Введите ваш пароль без спец. символов");
+string password = Console.ReadLine().ToLower();
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+while (true)
+{
+    test = "";
+    for (int i = 0; i < password.Length; i++)
+    {
+        Random rnd = new Random();
+        test += allow[rnd.Next(0, 36)];
+
+    }
+    Console.WriteLine(test);
+    if (test == password)
+    {
+        break;
+    }
+}
+
+stopwatch.Stop();
+TimeSpan ts = stopwatch.Elapsed;
+string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10);
+Console.WriteLine("RunTime " + elapsedTime);
+Console.ReadLine();
